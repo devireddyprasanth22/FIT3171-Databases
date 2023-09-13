15e7dd8b804ea6856fb44e196e78b8c45429fad7
github-devireddyprasanth22 <dpkrdrkr@gmail.com>
2023-09-13 10:59:33 +1000

    working on week 8 dml

---
+++ Applied/App08/week8_dml.sql
@@ -0,0 +1,54 @@
+/*
+Databases Week 8 Applied
+week8_dml.sql
+
+student id: 
+student name:
+last modified date:
+
+*/
+
+---==8.2.1 INSERT==--
+/*1 */
+
+---==8.2.2 INSERT using SEQUENCEs ==--
+/*1 */
+
+/*2 */
+
+
+---==8.2.3 Advanced INSERT==--
+/*1 */
+
+---==8.2.4 Creating a table and inserting data as a single SQL statement==--
+/*1 */
+
+/*2 */
+
+
+/*3 */
+
+---==8.2.5 UPDATE==--
+/*1 */
+
+
+/*2 */
+
+
+/*3 */
+
+
+/*4 */
+
+
+
+--==8.2.6 DELETE==--
+/*1 */
+
+
+/*2 */
+
+
+/*3 */
+
+
