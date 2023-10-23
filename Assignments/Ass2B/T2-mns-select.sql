035da72cae1942cc87d920aad774258bac484ea7
github-devireddyprasanth22 <dpkrdrkr@gmail.com>
2023-10-23 17:12:16 +1100

    Tasks 2a-2e complete

--- Assignments/Ass2B/T2-mns-select.sql
+++ Assignments/Ass2B/T2-mns-select.sql
@@ -29,7 +29,7 @@ ORDER BY item_stock DESC, item_id;
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-SELECT provider_title || '. ' || provider_fname || ' ' || provider_lname
+SELECT provider_title || '. ' || provider_fname || ' '|| provider_lname
 FROM mns.provider
 NATURAL JOIN mns.specialisation
 WHERE UPPER(mns.specialisation.spec_name) = 'PAEDIATRIC DENTISTRY'
@@ -39,20 +39,31 @@ ORDER BY provider_lname, provider_fname, provider_code;
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-
+SELECT service_code, service_desc, LPAD(TO_CHAR(service_stdfee, '$9999.99'), 15) as service_stdfee
+FROM mns.service
+WHERE service_stdfee > (SELECT AVG(service_stdfee) FROM mns.service)
+ORDER BY service_stdfee DESC, service_code;
 
 
 /*2(d)*/
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-
+SELECT appt_no, appt_datetime, patient_no, patient_fname, LPAD(TO_CHAR(apptserv_fee + COALESCE(apptserv_itemcost, 0),'$9999.99'), 15) as total
+FROM mns.appointment
+NATURAL JOIN mns.patient
+NATURAL JOIN mns.appt_serv 
+ORDER BY total DESC, appt_no;
 
 /*2(e)*/
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-
+SELECT service_code, service_desc, TO_CHAR(service_stdfee, '$9999.99') AS standard_fee,TO_CHAR(AVG(apptserv_fee) - service_stdfee, '$9999.99') AS service_fee_differential
+FROM mns.service
+NATURAL JOIN mns.appt_serv
+GROUP BY service_code, service_desc, service_stdfee
+ORDER BY service_code;
 
 
 /*2(f)*/
