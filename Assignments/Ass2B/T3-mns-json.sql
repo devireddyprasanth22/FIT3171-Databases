d5f116ad006a4a4dbb36a145f3d3f647db5ec188
github-devireddyprasanth22 <dpkrdrkr@gmail.com>
2023-10-24 19:06:15 +1100

    task 3a complete, need to verify output

--- Assignments/Ass2B/T3-mns-json.sql
+++ Assignments/Ass2B/T3-mns-json.sql
@@ -17,5 +17,24 @@
 -- PLEASE PLACE REQUIRED SQL SELECT STATEMENT TO GENERATE 
 -- THE COLLECTION OF JSON DOCUMENTS HERE
 -- ENSURE that your query is formatted and has a semicolon
--- (;) at the end of this answer
-
+-- (;) at the end of this answer APPOINTMENT, PROVIDER, ITEM, APPT_SERV, APPTSERVICE_ITEM
+set pagesize 300
+SELECT
+JSON_OBJECT(
+'_id' VALUE appt_no,
+'datetime' VALUE appt_datetime,
+'provider_code' VALUE provider_code,
+'item_totalcost' VALUE apptserv_itemcost,
+'no_of_items' VALUE as_item_quantity,
+'items' VALUE JSON_ARRAYAGG (JSON_OBJECT(
+'id' VALUE item_id,
+'desc' VALUE item_desc,
+'standardcost' VALUE item_stdcost,
+'quantity' VALUE 1) ORDER BY item_id) FORMAT JSON ) || ','
+FROM mns.appointment
+NATURAL JOIN mns.provider
+NATURAL JOIN mns.item
+NATURAL JOIN mns.appt_serv
+NATURAL JOIN mns.apptservice_item
+GROUP BY appt_no, appt_datetime, provider_code, apptserv_itemcost, as_item_quantity
+ORDER BY appt_no;
